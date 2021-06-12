d6156dd4b6ecf4b5d65d44634dcd40b52c8b6ff5
Chinmay Tagare <t.chinmaybhalchandra@gmail.com>
2021-06-12 22:59:20 +0530

    Decorator examples added

--- Src/Decorators.py
+++ Src/Decorators.py
@@ -1,7 +1,55 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 10 20:09:17 2021
-
-@author: tchin
+Topic  : Decorators in Python
+Author : Chinmay Tagare
+ 
 """
+
+def decorator_function(func):
+    print('in decorator function')
+    print(func.__name__)
+    def wrapper_function(*args, **kwargs):
+        print('inside wraper function')
+        return func(*args, **kwargs)
+    return wrapper_function
+    
+
+
+@decorator_function
+def display():
+    print('in display function')
+    var1 = 'variable in display function'
+    print(var1)
+    
+
+@decorator_function
+def display_info(name, age):
+    print(name, age)
+    
+
+display()
+print()
+display_info('John', 25)
+
+
+# Example of returning function from function
+"""
+def create_adder(x): 
+    def adder(y): 
+        return x+y 
+  
+    return adder 
+  
+add_15 = create_adder(15) 
+  
+print(add_15(10))
+
+"""
+
+
+
+
+
+
+
 
