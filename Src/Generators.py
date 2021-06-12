d976a54cc50fa71fc79d4da529ca72cd5d9f7a27
Chinmay Tagare <t.chinmaybhalchandra@gmail.com>
2021-06-12 18:41:46 +0530

    Included Generator example and description

--- Src/Generators.py
+++ Src/Generators.py
@@ -1,7 +1,55 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 10 20:09:17 2021
+Topic  : Generators in Python
+Author : Chinmay Tagare
 
-@author: tchin
 """
+
+import time
+
+# Traditional way 
+def square_numbers(nums):
+    result = []
+    for i in nums:
+        result.append(i*i)
+    return result
+
+my_nums = [i for i in range(10000000)]
+
+time1 = time.time()
+res = square_numbers(my_nums)
+print(time.time() - time1)
+# print(res)
+
+
+# use of generators
+# Generators don't hold entire result in memory
+# returns the generator object
+# It yields one result at a time when it is asked to
+def square_numbers_generator(nums):
+    for i in nums:
+        yield(i*i)
+
+my_nums_new = [i for i in range(10000000)]
+time1 = time.time()
+res_g = square_numbers_generator(my_nums_new)
+print(time.time() - time1)
+# print(res_g)
+
+
+# List comperhension
+
+nums_lst = [x*x for x in range(10)]
+print(nums_lst)
+
+nums_generator = (x*x for x in range(10))
+print(nums_generator)
+
+
+# INSIGHTS
+
+# GENERATORS ARE BETTER WITH PERFORMANCE FOR LARGE DATA
+
+
+
 
