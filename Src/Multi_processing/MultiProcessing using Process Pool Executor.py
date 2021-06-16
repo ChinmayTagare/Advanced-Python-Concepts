ea6ad3c46044d0f3ebcd68523d0c49960d244070
Chinmay Tagare <t.chinmaybhalchandra@gmail.com>
2021-06-16 19:58:15 +0530

    Included multipro method using process pool executor

--- Src/Multi_processing/MultiProcessing using Process Pool Executor.py
+++ Src/Multi_processing/MultiProcessing using Process Pool Executor.py
@@ -12,30 +12,44 @@ Author : Chinmay Tagare
 # _ is throw away variable 
 
 import time
-import multiprocessing
+import concurrent.futures
 
 start = time.perf_counter()
 
 def do_something(secs):
     print('Sleeping for {} seconds ... '.format(secs))
     time.sleep(secs)
-    print('Done sleeping... ')
+    return 'Done sleeping... '
 
+# lot less code using pool executor and list comperhension
 if __name__ == '__main__':
-    
-    process_list = []
-    for _ in range(10):
-        p = multiprocessing.Process(target=do_something, args=[1.5])
-        p.start()
-        process_list.append(p)
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = [executor.submit(do_something, 1) for _ in range(10)]
         
-    for process in process_list:
-        process.join()
+        # using as completed iterator
+        for f in concurrent.futures.as_completed(results):
+            print(f.result())
+# =============================================================================
+#     f_obj1 = executor.submit(do_something, 1)
+#         f_obj2 = executor.submit(do_something, 1)
+#         print(f_obj1.result())
+#         print(f_obj2.result())
+# =============================================================================
+    # =============================================================================
+    # process_list = []
+    # for _ in range(10):
+    #     p = multiprocessing.Process(target=do_something, args=[1.5])
+    #     p.start()
+    #     process_list.append(p)
+    #     
+    # for process in process_list:
+    #     process.join()
+    # 
+    # =============================================================================
     
+finish = time.perf_counter()
     
-    finish = time.perf_counter()
-    
-    print('Finished in {} seconds.'.format(round(finish-start, 2)))
+print('Finished in {} seconds.'.format(round(finish-start, 2)))
 
 
 
