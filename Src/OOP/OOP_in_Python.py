739b8f75a5a7ed3be1b3876e0388a3c1d2fc6d83
Chinmay Tagare <t.chinmaybhalchandra@gmail.com>
2021-06-16 20:58:46 +0530

    OOPs - added first class

--- Src/OOP/OOP_in_Python.py
+++ Src/OOP/OOP_in_Python.py
@@ -1,7 +1,32 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Jun 10 20:09:17 2021
-
-@author: tchin
+Python Object Oriented Programming
+Author : Chinmay Tagare
 """
 
+# class is blueprint to create an instances
+class Employee:
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+    
+
+emp1 = Employee('Chinmay', 'Tagare', 50000)
+emp2 = Employee('Test', 'User', 60000)
+
+# =============================================================================
+# emp1.first = 'Chinmay'
+# emp1.last = 'Tagare'
+# emp1.email = 'Chinmay.Tagare@company.com'
+# emp1.pay = 50000
+# 
+# emp2.first = 'Test'
+# emp2.last = 'User'
+# emp2.email = 'Test.User@company.com'
+# emp2.pay = 60000
+# 
+# =============================================================================
+print(emp1.email)
+print(emp2.email)
